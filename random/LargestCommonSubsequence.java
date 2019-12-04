876c5e92775d204ecd54330c6ded262b89f513a6
sumit.pathak <sumit.pathak@rakuten.com>
2019-12-04 13:27:58 +0530

    largest common subsequence DP java

--- random/LargestCommonSubsequence.java
+++ random/LargestCommonSubsequence.java
@@ -1,16 +1,16 @@
 package random;
 
-public class LeastCommonSubsequence {
+public class LargestCommonSubsequence {
 
     public static void main(String[] args) {
         String X = "ADFDSD";
         String Y = "BDCABA";
-        System.out.println(LCSLengthRecursive(X, Y, X.length(), Y.length()));
+        System.out.println(lcsLengthDPSolution(X, Y, X.length(), Y.length()));
     }
 
     // recursive solution
     public static int LCSLengthRecursive(String X, String Y, int m , int n) {
-        if(m==0 || n==0) return 0; // reached end of either of string
+        if(m==0 || n==0) return 0; // if any of the string is empty
         if(X.charAt(m-1)==Y.charAt(n-1)) { // last character matches
             return LCSLengthRecursive(X, Y, m-1, n-1) + 1;
         }
@@ -19,8 +19,14 @@ public class LeastCommonSubsequence {
 
     // dynamic programming solution
     public static int lcsLengthDPSolution(String X, String Y, int m, int n) {
-        int dp[][] = new int[m][n];
-        if(m==0 || n==0) return 0;
-        return -1;
+        int dp[][] = new int[m+1][n+1];
+        for(int i = 0; i<=m; i++) {
+            for(int j = 0; j<=n; j++) {
+                if(i==0 || j==0) dp[i][j] = 0;
+                else if(X.charAt(i-1)==Y.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
     }
 }
